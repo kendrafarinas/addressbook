88cf9d08247f024d07e0d32b9039fce477d16c92
visual_community@outlook.com <visual_community@outlook.com>
2019-08-24 11:02:48 -0400

    Solucion Completa

---
+++ mvcaAddress/mvcaAddress/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace mvcaAddress.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext    
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<mvcaAddress.Models.Book> Books { get; set; }
+    }
+}
